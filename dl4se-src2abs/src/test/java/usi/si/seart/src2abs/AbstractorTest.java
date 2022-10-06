1bf1786871d1adf790b7688ccceb7eaadcab2808
dabico <dabico@usi.ch>
2022-10-06 17:44:51 +0200

    Improved testing to be parametric

--- dl4se-src2abs/src/test/java/usi/si/seart/src2abs/AbstractorTest.java
+++ dl4se-src2abs/src/test/java/usi/si/seart/src2abs/AbstractorTest.java
@@ -6,13 +6,12 @@ import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Path;
-import java.util.stream.Stream;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 class AbstractorTest {
@@ -24,18 +23,22 @@ class AbstractorTest {
     Path inputs = Path.of(this.getClass().getResource("/inputs").getPath());
     Path idioms = Path.of(this.getClass().getResource("/idioms.csv").getPath());
 
-
-    @Test
-    void test() {
-        Stream.of(inputs.toFile().listFiles())
-                .map(File::toPath)
-                .forEach(input -> {
-                    Path name = input.getFileName();
-                    Path expected = outputs.resolve(name);
-                    Path actual = results.resolve(name);
-                    Abstractor.abstractCode(Parser.Granularity.CLASS, input, actual, idioms);
-                    Assertions.assertTrue(contentEquals(expected, actual));
-                });
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "01_main.java",
+            "02_regular.java",
+            "03_enum.java",
+            "04_interface.java",
+            "05_annotated.java",
+            "06_comments.java"
+    })
+    void test(String fileName) {
+        Path fileNamePath = Path.of(fileName);
+        Path input = inputs.resolve(fileNamePath);
+        Path expected = outputs.resolve(fileNamePath);
+        Path actual = results.resolve(fileNamePath);
+        Abstractor.abstractCode(Parser.Granularity.CLASS, input, actual, idioms);
+        Assertions.assertTrue(contentEquals(expected, actual));
     }
 
     @SneakyThrows
