94c02cb97d8f332c68d44e4971abcc4665d8a3df
dabico <dabico@usi.ch>
2022-11-27 14:33:51 +0100

    First iteration of `DateSelectInput` component
    Will be used for filtering purposes

---
+++ dl4se-website/src/components/DateSelectInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-input-group class="date-select-input">
+    <b-input type="text"
+             :id="id"
+             v-model.trim="input"
+             autocomplete="off"
+             :placeholder="placeholder"
+             class="date-select-input-field"
+    />
+    <b-input-group-append class="date-select-input-group">
+      <b-datepicker :id="`${id}-datepicker`" :aria-controls="id"
+                    v-model="calendar" @context="onContext" :min="min" :max="max" start-weekday="1"
+                    locale="en" :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
+                    selected-variant="secondary" button-variant="outline-secondary"
+                    class="date-select-input-datepicker" menu-class="date-select-input-datepicker-menu"
+                    hide-header button-only right :show-decade-nav="showDecadeNav"
+      />
+    </b-input-group-append>
+  </b-input-group>
+</template>
+
+<script>
+export default {
+  name: "b-date-select-input",
+  props: {
+    id: String,
+    value: {
+      type: String,
+      default: null
+    },
+    placeholder: String,
+    min: [Date, String],
+    max: [Date, String],
+  },
+  computed: {
+    showDecadeNav() {
+      const minMs = new Date(this.min).getTime()
+      const maxMs = new Date(this.max).getTime()
+      const difference = maxMs - minMs
+      return difference >= (1000 * 60 * 60 * 24 * 365)
+    }
+  },
+  methods: {
+    onContext(ctx) {
+      this.input = ctx.selectedYMD
+    }
+  },
+  watch: {
+    input() {
+      this.$emit('input', (this.input) ? this.input : null)
+    }
+  },
+  data() {
+    return {
+      input: this.value,
+      calendar: this.value
+    }
+  }
+}
+</script>
